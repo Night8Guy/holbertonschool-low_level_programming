e6ac6c7285fc76199251adfeb37127e61eb76be7
root <112408198+Night8Guy@users.noreply.github.com>
2023-02-07 08:14:30 -0800

    PLD FIX

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,17 +6,27 @@
  */
 
 void print_triangle(int size)
-{
-	int i, j;
 
-	for (i = 0; i < size; i++)
+{
+	int x, y, z;
+
+	if (size <= 0)
 	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
 	}
 	_putchar('\n');
+	}
+	}
 }
