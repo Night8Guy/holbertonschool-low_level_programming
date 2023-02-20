138ee487b3784cf6b947e6669dc80d1179ca3ff8
root <112408198+Night8Guy@users.noreply.github.com>
2023-02-20 12:41:14 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,5 @@
 #include "holberton.h"
 
-/**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
@@ -17,3 +11,4 @@ int _strlen_recursion(char *s)
 	}
 
 	return (len);
+}
