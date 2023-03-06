2a63027cd44e9e3154430f11ce65fd9f6c76dbbf
Night8Guy <robert@lionsroad.com>
2023-03-06 13:38:26 -0600

    Readme nad 1st task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+Typedef and Sturctures
+
+Typedef- shortens variable names
+Structures- ways to package variables
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef dog_h
+#define dog_h
+/**
+ *struct dog - experimental
+ *@name: Sparky
+ *@owner: Robby H
+ *@age: 3
+ *
+ */
+
+struct dog
+{
+	char *name, *owner;
+	float age;
+/* test comment. dog's traits are inconsequential*/
+
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+
+#endif
