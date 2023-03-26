3c74d3135d2b19f5959cd2641706dabf187d93cf
root <112408198+Night8Guy@users.noreply.github.com>
2023-03-26 07:32:51 -0700

    task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
+	unsigned long int i;
 
-	unsigned long int index = 0;
-
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-
-	if (!hash_table || !size)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (!hash_table->array)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	while (index++ < size)
-		hash_table->array[index] = NULL;
-
-	hash_table->size = size;
-	return (hash_table);
+	return (ht);
 }
