e21299bcb4c63f7df41e46f6e5db2fb18c9fc7e5
root <112408198+Night8Guy@users.noreply.github.com>
2023-03-26 07:45:08 -0700

    further reseach

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
 	unsigned long int i;
+	hash_table_t *new = NULL;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
-	return (ht);
+	for (i = 0 ; i < size ; i++)
+	{
+		new->array[i] = NULL;
+	}
+
+	return (new);
 }
